b15fd1142523500057636fd27cf7c56a9ae4cb45
ericlikedis <ereymundo2013@gmail.com>
2024-02-06 13:44:36 -0600

    updated code to include a new line ever yml row

--- csv2yml/csv2yml.py
+++ csv2yml/csv2yml.py
@@ -2,7 +2,7 @@ import csv
 import yaml
 
 # Defining variables for csv file path and the yml output file.
-csv_file_path = '' # Put file path to table here, may be easier to save or place the table into the same folder as your code. 
+csv_file_path = 'sample_data.csv'
 yaml_file_path = 'output.yaml'
 
 def csv_to_yaml(csv_file_path, yaml_file_path):
@@ -11,22 +11,25 @@ def csv_to_yaml(csv_file_path, yaml_file_path):
         # Read CSV data
         csv_reader = csv.DictReader(csv_file)
         
-        # Prepare data for YAML
-        yaml_data = []
-        for row in csv_reader:
-            yaml_row = {
-                'field': row.get('field', ),
-                'name': row.get('name', ),
-                'type': row.get('type', ),
-                'description': row.get('description', ),
-                'example': row.get('example', )
-            }
-            yaml_data.append(yaml_row)
+        with open(yaml_file_path, 'w') as yaml_file:
+
+        
+            # Prepare data for YAML
+            yaml_data = []
+            for row in csv_reader:
+                yaml_row = {
+                    'field': row.get('field', ),
+                    'name': row.get('name', ),
+                    'type': row.get('type', ),
+                    'description': row.get('description'),
+                    'example': row.get('example', )
+                }
+                yaml_data.append(yaml_row)
         
         # Write YAML data to file
-        with open(yaml_file_path, 'w') as yaml_file:
-            yaml.dump(yaml_data, yaml_file, default_flow_style=False, sort_keys=False)
-
+        
+                yaml.dump([yaml_row], yaml_file, default_flow_style=False, sort_keys=False) 
+                yaml_file.write('\n ')
 # Example usage
 
 csv_to_yaml(csv_file_path, yaml_file_path)
